0db7147dadb0a75b3e25edf1d8ba09f93a4e0e09
Eva Pace <eba.pachi@gmail.com>
2023-06-07 17:14:36 -0300

    support multiple folders

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Better Ls and Zicaaaaa.
 For computer science class.
 
 - [x] add proper suffix for each file type (`*`, `/`, `@`, `|`, `=`)
-- [ ] add color support
-- [ ] add support for multiple folders (`foo: ...\n bar: ...`)
+- [x] add support for multiple folders (`foo: ...\n bar: ...`)
 - [ ] show dot files (`-a`)
 - [ ] print files in alphabetic order
 - [ ] show all data about the files, eg: size, etc (`-l`)
+- [ ] add color support
--- main.c
+++ main.c
@@ -2,75 +2,106 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 
 int main(int argc, char* argv[]) {
-  char* foldername;
+  char **foldernames = argv;
 
   if (argc == 1) {
-    foldername = ".";
-  } else if (argc == 2) {
-    foldername = argv[1];
+    foldernames[0] = ".";
   } else {
-    fprintf(stderr, "blz doesn't support multiple folders yet\n");
-    exit(1);
+    foldernames = &argv[1];
+    argc -= 1;
   }
 
-  DIR *dir = opendir(foldername);
+  for (int i = 0; i < argc; i++) {
+    DIR *dir = opendir(foldernames[i]);
 
-  // it is actually a file
-  if (dir == NULL) {
-    printf("%s\n", foldername);
-    exit(0);
-  }
-
-  struct dirent *entry;
-
-  while ((entry = readdir(dir)) != NULL) {
-    if (entry->d_name[0] != '.') {
-      // print file name itself
-      printf("%s", entry->d_name);
-
-      // print file type suffix
-      switch (entry->d_type) {
-        case DT_DIR:
-          printf("/");
-          break;
-        case DT_FIFO:
-          printf("|");
-          break;
-        case DT_LNK:
-          printf("@");
-          break;
-        case DT_REG:
-          struct stat file_stat;
-
-          if (stat(entry->d_name, &file_stat) != 0) {
-            perror("Failed to get file status");
-            exit(errno);
-          }
-
-          // file is executable
-          if (file_stat.st_mode & S_IXUSR) {
-            printf("*");
-          }
-
-          break;
-        case DT_SOCK:
-          printf("=");
-          break;
-      }
-
-      // space in between files
-      printf(" ");
+    // it is actually a file
+    if (dir == NULL) {
+      printf("%s\n", foldernames[i]);
+      continue;
     }
-  }
 
-  printf("\n");
+    if (argc > 1) {
+      printf("%s:\n", foldernames[i]);
+    }
 
-  int close_res = closedir(dir);
+    struct dirent *entry;
 
-  if (close_res < 0) {
-    exit(errno);
+    while ((entry = readdir(dir)) != NULL) {
+      if (entry->d_name[0] != '.') {
+        // print file name itself
+        printf("%s", entry->d_name);
+
+        // print file type suffix
+        switch (entry->d_type) {
+          case DT_DIR:
+            printf("/");
+            break;
+          case DT_FIFO:
+            printf("|");
+            break;
+          case DT_LNK:
+            printf("@");
+            break;
+          case DT_REG:
+            char *full_path;
+
+            if (strcmp(foldernames[i], ".") != 0) {
+              int has_trailing_slash = foldernames[i][strlen(foldernames[i]) - 1] == '/';
+
+              full_path = malloc(strlen(foldernames[i]) + !has_trailing_slash + strlen(entry->d_name) + 1);
+
+              /* printf("foldernames[i]: %s\n", foldernames[i]); */
+              /* printf("entry->d_name: %s\n", entry->d_name); */
+
+              strcpy(full_path, foldernames[i]);
+              if (!has_trailing_slash) {
+                strcat(full_path, "/");
+              }
+              strcat(full_path, entry->d_name);
+            } else {
+              full_path = malloc(strlen(entry->d_name) + 1);
+
+              strcpy(full_path, entry->d_name);
+            }
+
+            struct stat file_stat;
+
+            if (stat(full_path, &file_stat) != 0) {
+              /* printf("\nfull_path: %s\n", full_path); */
+              free(full_path);
+              perror("Failed to get file status");
+              exit(errno);
+            }
+
+            free(full_path);
+
+            // file is executable
+            if (file_stat.st_mode & S_IXUSR) {
+              printf("*");
+            }
+
+            break;
+          case DT_SOCK:
+            printf("=");
+            break;
+        }
+
+        // space in between files
+        printf(" ");
+      }
+    }
+
+    printf("\n");
+
+    int close_res = closedir(dir);
+
+    if (close_res < 0) {
+      perror("Failed to close directory");
+      exit(errno);
+    }
   }
 }
