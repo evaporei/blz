e10fa6f281e0cccef68811f2163e66410fbcd722
Eva Pace <eba.pachi@gmail.com>
2023-06-08 12:37:25 -0300

    first part of -l (total number of blocks)

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ For computer science class.
 - [x] add proper suffix for each file type (`*`, `/`, `@`, `|`, `=`)
 - [x] add support for multiple folders (`foo: ...\n bar: ...`)
 - [x] show dot files (`-a`)
+- `-l`
+  - [x] show total number of blocks for the directory
+  - [ ] show all data about each file found (permissions, links, owner, group, size, last modified and name)
 - [ ] print files in alphabetic order
-- [ ] show all data about the files, eg: size, etc (`-l`)
 - [ ] add color support
--- main.c
+++ main.c
@@ -10,6 +10,8 @@ int main(int argc, char* argv[]) {
   char **foldernames;
   // to show dot files/folders
   bool flag_all = false;
+  // to show detailed data from each file and folder (permissions, size, etc)
+  bool flag_long_list_fmt = false;
 
   if (argc == 1) {
     foldernames[0] = ".";
@@ -21,6 +23,9 @@ int main(int argc, char* argv[]) {
         switch (arg[1]) {
           case 'a':
             flag_all = true;
+            break;
+          case 'l':
+            flag_long_list_fmt = true;
             break;
           default:
             printf("warning: unknown flag '-%c'\n", arg[1]);
@@ -36,6 +41,8 @@ int main(int argc, char* argv[]) {
   }
 
   for (int i = 0; i < argc; i++) {
+    unsigned long long total_blocks = 0;
+
     DIR *dir = opendir(foldernames[i]);
 
     // TODO: this can fail if file doesn't exist. It should check before `opendir`
@@ -60,6 +67,40 @@ int main(int argc, char* argv[]) {
       // print file name itself
       printf("%s", entry->d_name);
 
+      char *full_path;
+
+      if (strcmp(foldernames[i], ".") != 0) {
+        int has_trailing_slash = foldernames[i][strlen(foldernames[i]) - 1] == '/';
+
+        full_path = malloc(strlen(foldernames[i]) + !has_trailing_slash + strlen(entry->d_name) + 1);
+
+        /* printf("foldernames[i]: %s\n", foldernames[i]); */
+        /* printf("entry->d_name: %s\n", entry->d_name); */
+
+        strcpy(full_path, foldernames[i]);
+        if (!has_trailing_slash) {
+          strcat(full_path, "/");
+        }
+        strcat(full_path, entry->d_name);
+      } else {
+        full_path = malloc(strlen(entry->d_name) + 1);
+
+        strcpy(full_path, entry->d_name);
+      }
+
+      struct stat file_stat;
+
+      if (stat(full_path, &file_stat) != 0) {
+        /* printf("\nfull_path: %s\n", full_path); */
+        free(full_path);
+        perror("Failed to get file status");
+        exit(errno);
+      }
+
+      free(full_path);
+
+      total_blocks += (file_stat.st_blocks * 512) / 1024;
+
       // print file type suffix
       switch (entry->d_type) {
         case DT_DIR:
@@ -72,38 +113,6 @@ int main(int argc, char* argv[]) {
           printf("@");
           break;
         case DT_REG:
-          char *full_path;
-
-          if (strcmp(foldernames[i], ".") != 0) {
-            int has_trailing_slash = foldernames[i][strlen(foldernames[i]) - 1] == '/';
-
-            full_path = malloc(strlen(foldernames[i]) + !has_trailing_slash + strlen(entry->d_name) + 1);
-
-            /* printf("foldernames[i]: %s\n", foldernames[i]); */
-            /* printf("entry->d_name: %s\n", entry->d_name); */
-
-            strcpy(full_path, foldernames[i]);
-            if (!has_trailing_slash) {
-              strcat(full_path, "/");
-            }
-            strcat(full_path, entry->d_name);
-          } else {
-            full_path = malloc(strlen(entry->d_name) + 1);
-
-            strcpy(full_path, entry->d_name);
-          }
-
-          struct stat file_stat;
-
-          if (stat(full_path, &file_stat) != 0) {
-            /* printf("\nfull_path: %s\n", full_path); */
-            free(full_path);
-            perror("Failed to get file status");
-            exit(errno);
-          }
-
-          free(full_path);
-
           // file is executable
           if (file_stat.st_mode & S_IXUSR) {
             printf("*");
@@ -115,11 +124,19 @@ int main(int argc, char* argv[]) {
           break;
       }
 
-      // space in between files
-      printf(" ");
+      // separator for files
+      if (flag_long_list_fmt) {
+        printf("\n");
+      } else {
+        printf(" ");
+      }
     }
 
     printf("\n");
+
+    if (flag_long_list_fmt) {
+      printf("total: %llu\n", total_blocks);
+    }
 
     int close_res = closedir(dir);
 
