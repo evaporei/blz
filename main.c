b6ecf41179d9a7be859c54c205bc896f450a9f85
Eva Pace <eba.pachi@gmail.com>
2023-06-08 15:59:39 -0300

    remove unused parenthesis

--- main.c
+++ main.c
@@ -99,16 +99,16 @@ int main(int argc, char* argv[]) {
 
         if (flag_long_list_fmt) {
           // print file permissions
-          printf((S_ISDIR(file_stat.st_mode)) ? "d" : "-");
-          printf((file_stat.st_mode & S_IRUSR) ? "r" : "-");
-          printf((file_stat.st_mode & S_IWUSR) ? "w" : "-");
-          printf((file_stat.st_mode & S_IXUSR) ? "x" : "-");
-          printf((file_stat.st_mode & S_IRGRP) ? "r" : "-");
-          printf((file_stat.st_mode & S_IWGRP) ? "w" : "-");
-          printf((file_stat.st_mode & S_IXGRP) ? "x" : "-");
-          printf((file_stat.st_mode & S_IROTH) ? "r" : "-");
-          printf((file_stat.st_mode & S_IWOTH) ? "w" : "-");
-          printf((file_stat.st_mode & S_IXOTH) ? "x" : "-");
+          printf(S_ISDIR(file_stat.st_mode) ? "d" : "-");
+          printf(file_stat.st_mode & S_IRUSR ? "r" : "-");
+          printf(file_stat.st_mode & S_IWUSR ? "w" : "-");
+          printf(file_stat.st_mode & S_IXUSR ? "x" : "-");
+          printf(file_stat.st_mode & S_IRGRP ? "r" : "-");
+          printf(file_stat.st_mode & S_IWGRP ? "w" : "-");
+          printf(file_stat.st_mode & S_IXGRP ? "x" : "-");
+          printf(file_stat.st_mode & S_IROTH ? "r" : "-");
+          printf(file_stat.st_mode & S_IWOTH ? "w" : "-");
+          printf(file_stat.st_mode & S_IXOTH ? "x" : "-");
           printf(" ");
 
           // print number of hard links
